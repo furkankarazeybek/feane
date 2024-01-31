4d976b48624939c7c70001b554f3b611abfacccf
furkankarazeybek <furki304@gmail.com>
2024-01-31 04:24:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ schema/login.js
@@ -0,0 +1,9 @@
+import * as Yup from "yup";
+
+export const loginSchema = Yup.object({
+  email: Yup.string().required("Email is required.").email("Email is invalid."),
+  password: Yup.string()
+    .required("Password is required.")
+    .min(8, "Password must be at least 8 characters.")
+  
+});
